86c833d9e3699494b1ad26ae9e0f1f3cde166e9d
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2019-07-02 11:32:09 -0500

    Initial commit

---
+++ packages/dataviz.pkgs/R/package_logger.R
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------------------
+# dataviz.pkgs
+#
+# Package logger setup
+#----------------------------------------------------------------------------
+
+.logger_name <- "dataviz.pkgs"
+.pkg_logger <- logging::getLogger(.logger_name)
+.pkg_logger$setLevel("FINEST")
+
+pkg_loginfo <- function(msg, ...) tryCatch(logging::loginfo(msg, ..., logger = .pkg_logger),
+                                           error = function(e) warning(e))
+pkg_logdebug <- function(msg, ...) tryCatch(logging::logdebug(msg, ..., logger = .pkg_logger),
+                                            error = function(e) warning(e))
+pkg_logerror <- function(msg, ...) tryCatch(logging::logerror(msg, ..., logger = .pkg_logger),
+                                            error = function(e) warning(e))
+pkg_logwarn <- function(msg, ...) tryCatch(logging::logwarn(msg, ..., logger = .pkg_logger),
+                                           error = function(e) warning(e))
+pkg_logfinest <- function(msg, ...) tryCatch(logging::logfinest(msg, ..., logger = .pkg_logger),
+                                             error = function(e) warning(e))
+
+#'
+#' Retrieves dataviz.pkgs logger.
+#' 
+#' @return logger object
+#' 
+#' @export
+#' 
+dataviz.pkgs_getLogger <- function() {
+  .pkg_logger
+}
